343152ff13dafa5bec103278cad1b5baa0aba6d8
dotnavneet <dotnavneet@gmail.com>
2017-04-28 14:56:21 +0530

    dsfkl dsfl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello sdsfds nav {{name}}</h1>`
+  template: `<h1>Hello sdsfds gdkjgfjkjfdg fdgjklgf nav {{name}}</h1>`
 })
 export class AppComponent { name = 'Angular'; }
